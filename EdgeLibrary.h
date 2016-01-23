6ba1ae2ac2b609352615ebbb6a6425af5a178139
DoubleHub <someone50122015@gmail.com>
2016-01-23 23:49:34 +0100

    Added EdgeLibrary

---
+++ EdgeLibrary.h
@@ -0,0 +1,18 @@
+// EdgeLibrary.h
+// Includes all EdgeLibrary headers
+
+#pragma once
+// INCLUDE SPACE ====================================
+#define WIN32_LEAN_AND_MEAN
+#include "MemoryManager.h"
+#include "OffsetManager.h"
+#include "ResourceManager.h"
+#include "LogManager.h"
+#include "ClientManager.h"
+#include "Vector2.h"
+#include "Vector3.h"
+#include "LibraryManager.h"
+#include "LocalPlayerManager.h"
+#include "EntityManager.h"
+#include "GlowObjectPlayer.h"
+// ==================================================
