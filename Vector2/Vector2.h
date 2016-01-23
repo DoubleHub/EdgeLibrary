d27b0e0fa7c98cfff6b2f703e3ea631eb7f10a9b
DoubleHub <someone50122015@gmail.com>
2016-01-23 23:48:05 +0100

    Added Vector2 and Vector3

---
+++ Vector2/Vector2.h
@@ -0,0 +1,18 @@
+// Vector2.h
+// Declares Vector2 class
+
+#pragma once
+
+// INCLUDE SPACE =========================================
+#include "stdafx.h"
+// =======================================================
+
+namespace EdgeLibrary
+{
+	//A vector containing 2 floats
+	class Vector2
+	{
+	public:
+		float x, y;
+	};
+}
---
+++ Vector3/Vector3.h
@@ -0,0 +1,18 @@
+// Vector3.h
+// Declares Vector3 class
+
+#pragma once
+
+// INCLUDE SPACE =========================================
+#include "stdafx.h"
+// =======================================================
+
+namespace EdgeLibrary
+{
+	//A vector containing 3 floats
+	class Vector3
+	{
+	public:
+		float x, y, z;
+	};
+}
