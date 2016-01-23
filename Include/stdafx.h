8d5e054d876626b6b158b8b74a8b97e1f5bf1556
DoubleHub <someone50122015@gmail.com>
2016-01-23 23:49:06 +0100

    Added stdafx

---
+++ Include/stdafx.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <direct.h>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <stdint.h>
+#include <unordered_map>
+
+#include "MemoryManager.h"
+#include "OffsetManager.h"
+#include "ResourceManager.h"
+#include "LogManager.h"
+#include "LibraryManager.h"
+#include "LocalPlayerManager.h"
+#include "ClientManager.h"
+#include "EntityManager.h"
+#include "CHEntityManager.h"
+#include "GlowObjectPlayer.h"
+#include "FileManager.h"
+#include "ValveManager.h"
+#include "SkinManager.h"
